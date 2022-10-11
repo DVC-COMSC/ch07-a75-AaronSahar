56c4a0d992416e0326e476f159447b94c33680a9
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 22:55:35 -0700

    test

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import sys
 
 num1 = list(map(int, input("Enter the numbers ").split()))
 num2 = list(map(int, input("Enter the numbers ").split()))
-match = False
-loop = 1
 if len(num1) > len(num2):
 	print ('False')
 	sys.exit(0)
-if len(num1) <= len(num2) and set(num2) <= set(num1):
-		match=True
-print(match)
+print(set(num2).issubset(set(num1)))
