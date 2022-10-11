740799f1fcef3aef0bcc4da5bb5a56402ccd1242
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 22:56:59 -0700

    test

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ num2 = list(map(int, input("Enter the numbers ").split()))
 if len(num1) > len(num2):
 	print ('False')
 	sys.exit(0)
-print(set(num2).issubset(set(num1)))
+print(set(num1).issubset(set(num2)))
