e7357db84de44aa66386dc1fbc11c923b67d111f
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 22:46:54 -0700

    test

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-num1 = list(map(int, input("Enter the numbers ").split()))
-num2 = list(map(int, input("Enter the numbers ").split()))
+
+import sys
+
+num1 = list(map(int, input().split()))
+num2 = list(map(int, input().split()))
+
 match = False
 loop = 1
-if len(num1) >= len(num2):
-    for i in range (len(num1)):
+
+if len(num1) > len(num2):
+	print ('False')
+	sys.exit(0)
+for i in range (len(num1)):
         if num2[0] == num1[i]:
             match = True
             while i+loop < len(num2):
                 if num2[loop] != num1[i+loop]:
                     match = False
                 loop += 1
-print(match)
+# ******************************
+# Make your Code
+# ******************************
+
+# print ('True') or ('False')
