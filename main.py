9d71fbb54bffad3c129c69155d4647183aee041d
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 22:48:39 -0700

    test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ loop = 1
 if len(num1) > len(num2):
 	print ('False')
 	sys.exit(0)
-if len(num1) >= len(num2):
+if len(num1) <= len(num2):
     for i in range (len(num1)):
         if num2[0] == num1[i]:
             match = True
